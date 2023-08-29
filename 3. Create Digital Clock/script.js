11e53459ea8a80afd102369bec47d0ac2735db72
Pradeep Pal <131147634+Palp0987@users.noreply.github.com>
2023-08-29 10:01:13 +0530

    "mew Updated FIles"

--- 3. Create Digital Clock/script.js
+++ 3. Create Digital Clock/script.js
@@ -20,3 +20,40 @@ setInterval(() => {
   sec.innerHTML =
     (currentTime.getSeconds() < 10 ? "0" : "") + currentTime.getSeconds();
 }, 1000);
+
+/*
+The code snippet is responsible for updating the HTML elements with 
+the current time every second.
+
+Flow--------------
+
+The code snippet first retrieves the HTML elements with the 
+IDs "hrs", "min", and "sec" using the getElementById method.
+
+
+It then sets up a setInterval function that will execute the provided 
+callback function every 1000 milliseconds (1 second).
+
+
+Inside the callback function, it creates a new Date object to get the 
+current time.
+
+
+It updates the innerHTML of the "hrs" element with the current hour, 
+ensuring that it is displayed with two digits (e.g., 01, 02, etc.).
+
+
+It updates the innerHTML of the "min" element with the current minute, 
+also ensuring that it is displayed with two digits.
+
+
+It updates the innerHTML of the "sec" element with the current second, 
+again ensuring that it is displayed with two digits.
+
+
+The setInterval function continues to execute the callback function every 
+second, keeping the displayed time updated.
+
+
+
+*/
